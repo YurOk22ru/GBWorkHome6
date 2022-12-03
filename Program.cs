55cc2352c0f7d3a88626f3771f5f5e0e204ee6f8
YurOk22ru <yurok-potolok@mail.ru>
2022-12-03 09:51:00 +0300

    триугольник

--- Program.cs
+++ Program.cs
@@ -1,103 +1,56 @@
-﻿// Задача: 
-// Написать программу сложения и вычитания двух многочленов
-
-
-
-
-// f(x) =  1*x^0 + 2*x^1 + 0*x^2 + 4*x^3 + 5*x^4+ 16*x^5
-// g(x) =  10*x^0 + 11*x^1 + 4*x^2 
-
-// Метод подсчета суммы многочленов
-int[] Sum(int[] f, int[] g)
+﻿int[,] CreateTriangle(int row)
 {
-  int powF = f.Length;
-  int powG = g.Length;
-
-  int resultMax = powF;
-  int resultMin = powG;
-
-  if (powG > resultMax)
+  int[,] triangle = new int[row, row];
+  for (int i = 0; i < row; i++)
   {
-    resultMax = powG;
-    resultMin = powF;
+    triangle[i, 0] = 1;
+    triangle[i, i] = 1;
   }
 
-  int[] result = new int[resultMax];
-
-  for (int i = 0; i < resultMin; i++)
+  for (int i = 2; i < row; i++)
   {
-    result[i] = f[i] + g[i];
+    for (int j = 1; j <= i; j++)
+    {
+      triangle[i, j] =
+      triangle[i - 1, j - 1] + triangle[i - 1, j];
+    }
   }
-
-  for (int i = resultMin; i < resultMax; i++)
-  {
-    if (resultMax == powG) result[i] = g[i];
-    else result[i] = f[i];
-  }
-
-  return result;
+  return triangle;
 }
 
-// Метод подсчета разницы многочленов
-int[] Dif(int[] f, int[] g)
+void PrintTriangle(int[,] triangle)
 {
-  int powF = f.Length;
-  int powG = g.Length;
-
-  int resultMax = powF;
-  int resultMin = powG;
-
-  if (powG > resultMax)
+  int row = triangle.GetLength(0);
+  for (int i = 0; i < row; i++)
   {
-    resultMax = powG;
-    resultMin = powF;
+    for (int j = 0; j < row; j++)
+    {
+      if (triangle[i, j] != 0)
+        Console.Write($"{triangle[i, j],4}");
+    }
+    Console.WriteLine();
   }
-
-  int[] result = new int[resultMax];
-
-  for (int i = 0; i < resultMin; i++)
-  {
-    result[i] = f[i] - g[i];
-  }
-
-  for (int i = resultMin; i < resultMax; i++)
-  {
-    if (resultMax == powG) result[i] = -g[i];
-    else result[i] = f[i];
-  }
-
-  return result;
 }
 
-// Метод вывода элементов многочлена на экран
-string Print(int[] f)
+int[] GetKoeff(int[,] tr, int pow)
 {
-  // string[] pows = { "", "", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹" };
-  string[] pows = { "^0", "^1", "^2", "^3", "^4", "^5", "^6", "^7", "^8", "^9" };
-  string output = String.Empty;
-  for (int i = 0; i < f.Length; i++)
+  int[] row = new int[pow + 1];
+  for (int i = 0; i <= pow; i++)
   {
-    int t = f[i];
-    if (f[i] == 0) continue;
-    if (f[i] < 0) { output += " - "; }
-    else if (i != 0) { output += " + "; }
-
-    if (t < 0) t = -t;
-    if (i == 1) { output += $"{t}x"; }
-    if (i == 0) { output += $"{t}"; }
-    if (i != 1 && i != 0 && f[i] != 0) { output += $"{t}x{pows[i]}"; }
-    //if (flag && f[i] != 0 && i < f.Length - 1) output += " + ";
+    row[i] = tr[pow, i];
   }
-
-  return output;
+  return row;
 }
 
-int[] f = { 1, 0, 0, 0, -9, -6  };
-int[] g = {0, 1, -2, -5, 0, 0, 7, 3 };
-int[] s = Sum(f, g);
-int[] d = Dif(f, g);
-Console.WriteLine($"f(x) = {Print(f)}");
-Console.WriteLine($"g(x) = {Print(g)}");
-System.Console.WriteLine();
-Console.WriteLine($"f(x) + g(x) = {Print(s)}");
-Console.WriteLine($"f(x) - g(x) = {Print(d)}");
+int[,] tr = CreateTriangle(10);
+PrintTriangle(tr);
+int[] koeff = GetKoeff(tr, 2);
+
+System.Console.WriteLine(String.Join(' ', koeff));
+
+System.Console.WriteLine(koeff.Length);
+
+for (int i = 0; i < koeff.Length; i++)
+{
+  Console.Write($"{koeff[i]}*a^{koeff.Length - 1 - i}*b^{i} + ");
+}
