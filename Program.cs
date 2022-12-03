da9f5ae93f64ba4cc790d2a9219c5bc6e6d9cfe7
YurOk22ru <yurok-potolok@mail.ru>
2022-12-03 09:57:07 +0300

    триугольник

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿//Доделать ввод многочлена,
-//Сделать вывод только нечетных коэффициентов у треугольника распечатайте хотя бы 120 строк
-
-int[,] CreateTriangle(int row)
+﻿int[,] CreateTriangle(int row)
 {
   int[,] triangle = new int[row, row];
   for (int i = 0; i < row; i++)
@@ -23,43 +20,16 @@ int[,] CreateTriangle(int row)
 
 void PrintTriangle(int[,] triangle)
 {
-  int row = triangle.GetLength(0);
-  for (int i = 0; i < row; i++)
+    for (int i = 0; i < triangle.GetLength(0); i++)
   {
-    for (int j = 0; j < row; j++)
+    for (int j = 0; j < triangle.GetLength(1); j++)
     {
-      if (triangle[i, j] %2 !=0)
-        //Console.Write($"{triangle[i, j],4}");
-        Console.Write("*");
-      else
-      {
-        System.Console.Write(" ");
-      }  
-
+      if (triangle[i, j] % 2 == 0) Console.Write(" ");
+      else Console.Write("*");
     }
     Console.WriteLine();
   }
 }
 
-int[] GetKoeff(int[,] tr, int pow)
-{
-  int[] row = new int[pow + 1];
-  for (int i = 0; i <= pow; i++)
-  {
-    row[i] = tr[pow, i];
-  }
-  return row;
-}
-
-int[,] tr = CreateTriangle(120);
+int[,] tr = CreateTriangle(120);           
 PrintTriangle(tr);
-int[] koeff = GetKoeff(tr, 2);
-
-System.Console.WriteLine(String.Join(' ', koeff));
-
-System.Console.WriteLine(koeff.Length);
-
-for (int i = 0; i < koeff.Length; i++)
-{
-  Console.Write($"{koeff[i]}*a^{koeff.Length - 1 - i}*b^{i} + ");
-}
